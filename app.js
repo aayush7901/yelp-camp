af94b28f947a0ab376b39e947c62ca2946d2b2b1
Aayush <aayush7901@gmail.com>
2020-06-25 17:01:31 +0530

    update port

--- app.js
+++ app.js
@@ -47,7 +47,5 @@ app.use(indexRoutes);
 app.use(campgroundRoutes);
 app.use(commentRoutes);
 
-app.listen(3000,function(){
-    console.log("The YelpCamp Server Has Started");
-})
+app.listen(process.env.PORT);
 
